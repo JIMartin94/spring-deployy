65423b765af83c1b07cde2ddb77abce575af5164
juaniMartin <juani-martin@hotmail.com>
2022-11-19 17:37:51 -0300

    spring heroku app config

---
+++ src/main/java/com/example/springdeployy/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.springdeployy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hola")
+    public String holaMundo(){ return "Hola mundo que tal vamos!! hasta luego!";}
+}
---
+++ src/main/java/com/example/springdeployy/SpringDeployyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdeployy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeployyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDeployyApplication.class, args);
+	}
+
+}
